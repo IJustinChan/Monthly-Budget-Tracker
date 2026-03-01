a0eb2186d7931ee2b8652e1a75bf0f6fb229ef1c
Justin Chan <ijustinc@students.cs.ubc.ca>
2026-02-28 16:09:11 -0800

    added another test for JsonWriter

--- src/test/persistance/JsonReaderTest.java
+++ src/test/persistance/JsonReaderTest.java
@@ -31,12 +31,12 @@ public class JsonReaderTest extends JsonTest {
     void testReaderEmptyBudget() {
         JsonReader reader = new JsonReader("./data/testReaderBudgetEmpty.json");
         try {
-            Budget wr = reader.read();
-            assertEquals(1, wr.getMonth());
-            assertEquals(2026, wr.getYear());
-            assertFalse(wr.getIsInScenarioMode());
-            checkMonthlyBudget(1, 2026, 0, 0, wr.getCurrentMonthBudget());
-            checkMonthlyBudget(1, 2026, 0, 0, wr.getScenarioAddOns());
+            Budget budget = reader.read();
+            assertEquals(1, budget.getMonth());
+            assertEquals(2026, budget.getYear());
+            assertFalse(budget.getIsInScenarioMode());
+            checkMonthlyBudget(1, 2026, 0, 0, budget.getCurrentMonthBudget());
+            checkMonthlyBudget(1, 2026, 0, 0, budget.getScenarioAddOns());
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
@@ -46,15 +46,15 @@ public class JsonReaderTest extends JsonTest {
     void testReaderGeneralBudget() {
         JsonReader reader = new JsonReader("./data/testReaderGeneralBudget.json");
         try {
-            Budget wr = reader.read();
-            assertEquals(1, wr.getMonth());
-            assertEquals(2026, wr.getYear());
-            MonthlyBudget currentMonthlyBudget = wr.getCurrentMonthBudget();
+            Budget budget = reader.read();
+            assertEquals(1, budget.getMonth());
+            assertEquals(2026, budget.getYear());
+            MonthlyBudget currentMonthlyBudget = budget.getCurrentMonthBudget();
             checkMonthlyBudget(1, 2026, 2, 1, currentMonthlyBudget);
             checkIncome(100, 3, "work", 0.1, currentMonthlyBudget.getAllIncome().get(0));
             checkIncome(1000, 20, "scholarship", 0, currentMonthlyBudget.getAllIncome().get(1));
             checkExpense(50, 25, "food", "need", currentMonthlyBudget.getAllExpenses().get(0));
-            MonthlyBudget scenarioAddOns = wr.getScenarioAddOns();
+            MonthlyBudget scenarioAddOns = budget.getScenarioAddOns();
             checkMonthlyBudget(1, 2026, 1, 1, scenarioAddOns);
             checkIncome(2500, 27, "scholarship", 0, scenarioAddOns.getAllIncome().get(0));
             checkExpense(1400, 1, "rent", "need", scenarioAddOns.getAllExpenses().get(0));
--- src/test/persistance/JsonWriterTest.java
+++ src/test/persistance/JsonWriterTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
 import model.Budget;
+import model.Expense;
+import model.Income;
+import model.MonthlyBudget;
 
 import java.io.IOException;
 import java.util.List;
@@ -53,7 +56,13 @@ public class JsonWriterTest extends JsonTest {
     void testWriterGeneralWorkroom() {
         try {
             Budget budget = new Budget();
-            // setup here
+            budget.setup(2, 2026);
+            budget.getCurrentMonthBudget().addIncome(new Income(100, 3, "work", 0.1));
+            budget.getCurrentMonthBudget().addIncome(new Income(1000, 20, "scholarship", 0));
+            budget.getCurrentMonthBudget().addExpense(new Expense(50, 25, "food", "need"));
+
+            budget.getScenarioAddOns().addIncome(new Income(2500, 27, "scholarship", 0));
+            budget.getScenarioAddOns().addExpense(new Expense(1400, 1, "rent", "need"));
 
             JsonWriter writer = new JsonWriter("./data/testWriterGeneralBudget.json");
             writer.open();
@@ -61,9 +70,18 @@ public class JsonWriterTest extends JsonTest {
             writer.close();
 
             JsonReader reader = new JsonReader("./data/testWriterGeneralBudget.json");
-            // budget = reader.read();
-            // assert statements here
-            // handle later
+            budget = reader.read();
+            assertEquals(1, budget.getMonth());
+            assertEquals(2026, budget.getYear());
+            MonthlyBudget currentMonthlyBudget = budget.getCurrentMonthBudget();
+            checkMonthlyBudget(1, 2026, 2, 1, currentMonthlyBudget);
+            checkIncome(100, 3, "work", 0.1, currentMonthlyBudget.getAllIncome().get(0));
+            checkIncome(1000, 20, "scholarship", 0, currentMonthlyBudget.getAllIncome().get(1));
+            checkExpense(50, 25, "food", "need", currentMonthlyBudget.getAllExpenses().get(0));
+            MonthlyBudget scenarioAddOns = budget.getScenarioAddOns();
+            checkMonthlyBudget(1, 2026, 1, 1, scenarioAddOns);
+            checkIncome(2500, 27, "scholarship", 0, scenarioAddOns.getAllIncome().get(0));
+            checkExpense(1400, 1, "rent", "need", scenarioAddOns.getAllExpenses().get(0));
 
         } catch (IOException e) {
             fail("Exception should not have been thrown");
